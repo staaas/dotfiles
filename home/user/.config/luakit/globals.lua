12db0341854b964ba7a2faa199d35a31f2803476
Stas Rudakou <stas@garage22.net>
2012-09-29 09:24:10 +0300

    Edit luakit globals: homepage and search engines

--- home/user/.config/luakit/globals.lua
+++ home/user/.config/luakit/globals.lua
@@ -1,6 +1,6 @@
 -- Global variables for luakit
 globals = {
-    homepage            = "http://luakit.org/",
+    homepage            = "about:blank",
  -- homepage            = "http://github.com/mason-larobina/luakit",
     scroll_step         = 40,
     zoom_step           = 0.1,
@@ -52,14 +52,14 @@ soup.accept_policy = cookie_policy.always
 -- See: http://www.lua.org/manual/5.1/manual.html#pdf-string.format
 search_engines = {
     duckduckgo  = "https://duckduckgo.com/?q=%s",
-    github      = "https://github.com/search?q=%s",
-    google      = "https://google.com/search?q=%s",
+    gh          = "https://github.com/search?q=%s",
+    g           = "https://google.com/search?q=%s",
     imdb        = "http://www.imdb.com/find?s=all&q=%s",
-    wikipedia   = "https://en.wikipedia.org/wiki/Special:Search?search=%s",
+    w           = "https://en.wikipedia.org/wiki/Special:Search?search=%s",
 }
 
 -- Set google as fallback search engine
-search_engines.default = search_engines.google
+search_engines.default = search_engines.g
 -- Use this instead to disable auto-searching
 --search_engines.default = "%s"
 
