6a833e6baef7be1819c845019682151f76ef8bdf
Stas Rudakou <stas@garage22.net>
2015-01-09 08:37:04 +0300

    Render HTML email in mu4e

--- templates/home/user/.emacs.d/mail.el
+++ templates/home/user/.emacs.d/mail.el
@@ -9,3 +9,11 @@
  mu4e-refile-folder "/Archive")   ;; saved messages
 
 (setq mu4e-view-show-addresses t)  ;; show email addresses along with names
+
+;; rendering html
+(defun my-render-html-message ()
+  (let ((dom (libxml-parse-html-region (point-min) (point-max))))
+    (erase-buffer)
+    (shr-insert-document dom)
+    (goto-char (point-min))))
+(setq mu4e-html2text-command 'my-render-html-message)
