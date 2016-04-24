bb198e5b5a24822adeb20e1022e0014a279b3a3c
Stas Rudakou <stas@garage22.net>
2016-04-24 17:51:36 +0300

    Add emacs role

---
+++ roles/emacs/templates/.emacs.d/org.el
@@ -0,0 +1,4 @@
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
---
+++ roles/emacs/templates/.emacs.d/packaging.el
@@ -0,0 +1,56 @@
+;; make more packages available with the package installer
+(setq to-install
+      '(
+	ace-window  ;; switching between windows
+	flycheck
+	ein
+	magit
+	yasnippet
+	autopair
+	find-file-in-repository
+	projectile
+	neotree
+	company
+	ag
+	;; passwords & encryption
+	password-store
+	auth-password-store
+	;; python
+	python-mode
+	anaconda-mode
+	company-anaconda
+	;; lisp
+	slime
+	;; rust
+	rust-mode
+	company-racer
+	racer
+	flycheck-rust
+	rust-mode
+	cargo
+	rustfmt
+	))
+
+(require 'package)
+(package-initialize)
+
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(defun install-if-needed (package)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(defun my-filter (condp lst)
+  (delq nil
+	(mapcar (lambda (x) (and (funcall condp x) x)) lst)))
+
+(message "%s" "Refreshing package index")
+(if (my-filter (lambda (package) (not (package-installed-p package))) to-install)
+    (package-refresh-contents))
+
+(message "%s" "Installing packages...")
+(mapc 'install-if-needed to-install)
+(message "%s" " done\n")
