7b23041d7a5e44584f7ec519e4b20747668e743c
Stas Rudakou <stas@garage22.net>
2016-05-03 10:35:07 +0300

    Remove migrated playbooks and templates from old layout

--- templates/home/user/mail/offlineimap.py
+++
@@ -1,9 +0,0 @@
-#!/usb/bin/python
-from subprocess import Popen, PIPE
-
-
-def get_password(name):
-    """ Returns password, retrieved from gnome keyring """
-    cmd = ["gnome-keyring-query", "get", name]
-    stdout, stderr = Popen(cmd, stdout=PIPE).communicate()
-    return stdout.strip()
