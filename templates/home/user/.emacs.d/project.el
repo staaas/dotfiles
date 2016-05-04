e9ceb2eac60e706838046b2ad8e78b1e284c49bc
Stas Rudakou <stas@garage22.net>
2016-05-04 13:19:05 +0300

    Remove migrated playbooks and templates from old layout

--- templates/home/user/.emacs.d/project.el
+++
@@ -1,7 +0,0 @@
-(require 'projectile)
-(projectile-global-mode)
-
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
-
-(setq projectile-switch-project-action 'neotree-projectile-action)
--- templates/home/user/.emacs.d/startup.el
+++
@@ -1,5 +0,0 @@
-;; Get rid of "GNU Emacs" buffer at startup
-(setq inhibit-start-screen t) 
-(setq inhibit-splash-screen t) 
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
