eb44d46f8289f31ec4abb5be86dc1d71fc1877e5
Stas Rudakou <stas@garage22.net>
2016-05-10 18:15:36 +0300

    desktop role: spawn light-locker on startup

--- roles/desktop/templates/awesome/rc.lua
+++ roles/desktop/templates/awesome/rc.lua
@@ -392,4 +392,5 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
-awful.util.spawn("sbxkb");
+awful.util.spawn("sbxkb")
+awful.util.spawn("light-locker")
