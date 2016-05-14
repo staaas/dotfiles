496d2a62dcda43f68f0644a80a0fa5233442a0b6
Stas Rudakou <stas@garage22.net>
2016-05-14 19:20:13 +0300

    awesome: use tile layout as a defualt

--- roles/desktop/templates/awesome/rc.lua
+++ roles/desktop/templates/awesome/rc.lua
@@ -57,7 +57,6 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts =
 {
-    awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
@@ -68,7 +67,8 @@ local layouts =
     awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier
+    awful.layout.suit.magnifier,
+    awful.layout.suit.floating
 }
 -- }}}
 
