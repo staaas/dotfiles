c1e74f5f70bdb143255b4f91bfd6d6a81e1dab55
Stas Rudakou <stas@garage22.net>
2018-08-20 14:37:28 +0300

    emacs (org mode): add a way to open links in Chrome

--- roles/emacs/templates/.emacs.d/org.el
+++ roles/emacs/templates/.emacs.d/org.el
@@ -3,3 +3,7 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 (setq org-agenda-files '("~/org"))
+
+;; Add a way to open links in Chrome (which is non-default browser)
+;; by prefixing URLs with chrome:, e.g. chrome:https://mozilla.org
+(org-link-set-parameters "chrome" :follow (lambda (path) (browse-url-chrome path)))
