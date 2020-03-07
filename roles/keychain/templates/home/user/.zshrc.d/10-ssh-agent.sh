e8f17b43f316e3dd2586cbbded9f74ef448d4421
Stas Rudakou <stas@garage22.net>
2020-03-07 16:33:47 +0300

    Switch from zsh to fish

--- roles/keychain/templates/home/user/.zshrc.d/10-ssh-agent.sh
+++
@@ -1,2 +0,0 @@
-export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
-export GPG_TTY=$(tty)
---
+++ roles/keychain/templates/ssh-agent.fish
@@ -0,0 +1,2 @@
+set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh"
+set -x GPG_TTY (tty)
