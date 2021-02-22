929d2f3f0b6d05bc668526d0d217126f5ce828bf
Stas Rudakou <stas@garage22.net>
2021-02-22 17:47:25 +0300

    Switch from pulse to pipewire

--- roles/desktop/templates/sway/sway.fish
+++ roles/desktop/templates/sway/sway.fish
@@ -3,5 +3,8 @@ if test -z $DISPLAY; and test (tty) = "/dev/tty1"
   # enforce Wayland for Firefox
   set -x MOZ_ENABLE_WAYLAND 1
 
+  # use xdg-desktop-portal-wlr
+  set -x XDG_CURRENT_DESKTOP sway
+
   exec sway
 end
