9c3a52dd0a20934020a5ba18fc0d6de0564f2a9e
Stas Rudakou <stas@garage22.net>
2021-11-28 00:49:56 +0300

    sway: set WLR_NO_HARDWARE_CURSORS to 1 in an attempt to avoid cursor disappearing occasioanally

--- roles/desktop/templates/sway/sway.fish
+++ roles/desktop/templates/sway/sway.fish
@@ -3,6 +3,10 @@ if test -z $DISPLAY; and test (tty) = "/dev/tty1"
   # enforce Wayland for Firefox
   set -x MOZ_ENABLE_WAYLAND 1
 
+  # switching to a software cursor instead of a hardware one,
+  # cursor sometimes disappears without this
+  set -x WLR_NO_HARDWARE_CURSORS 1
+
   # use xdg-desktop-portal-wlr
   set -x XDG_CURRENT_DESKTOP sway
 
