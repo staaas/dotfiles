101895aedb744321be9f9991ad8196ef6edaf330
Stas Rudakou <stas@garage22.net>
2022-01-16 16:00:17 +0300

    desktop: configure screen sharing via xdg-desktop-portal-wlr in sway

--- roles/desktop/templates/sway/sway.fish
+++ roles/desktop/templates/sway/sway.fish
@@ -7,7 +7,8 @@ if test -z $DISPLAY; and test (tty) = "/dev/tty1"
   # cursor sometimes disappears without this
   set -x WLR_NO_HARDWARE_CURSORS 1
 
-  # use xdg-desktop-portal-wlr
+  # use xdg-desktop-portal-wlr for screen sharing
+  # (we will also need to include /etc/sway/config.d/50-systemd-user.conf in sway config)
   set -x XDG_CURRENT_DESKTOP sway
 
   exec sway
