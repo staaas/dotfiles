3d43a312f3605e7aac6372cf7a2c9633f7ce7f25
Stas Rudakou <stas@garage22.net>
2024-10-05 14:34:02 +0200

    sway: switch from light to acpilight, get rid of unused clamshell mode, update default font sizes

--- roles/desktop/templates/sway/restore-lid-state.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-laptopscreen=${1:-eDP-1}
-if grep -q open /proc/acpi/button/lid/LID/state; then
-    swaymsg output $laptopscreen enable
-else
-    swaymsg output $laptopscreen disable
-fi
